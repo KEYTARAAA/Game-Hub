a8494e7bf563b40ade3a5a79cb34ff35d07ffe2a
KITARA STEWART <18241476@studentmail.ul.ie>
2024-02-26 22:11:22 +0000

    Create generic fetch hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,11 @@
-import useGenres from "../hooks/useGenres";
+import useGenres, { Genre } from "../hooks/useGenres";
 
 function GenreList() {
-  const { genres, error, isLoading } = useGenres();
+  const { data } = useGenres();
   return (
     <ul>
-      {genres.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+      {data.map((d) => (
+        <li key={d.id}>{d.name}</li>
       ))}
     </ul>
   );
