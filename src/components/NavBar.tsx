2d7d71ce548610cdf8bb3824d1c8572ea373d8c8
KITARA STEWART <18241476@studentmail.ul.ie>
2024-02-26 14:20:59 +0000

    Build a Navbar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import { GridItem, HStack, Image, Text } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+
+function NavBar() {
+  return (
+    <HStack>
+      <Image src={logo} boxSize="60px" />
+      <Text>
+        <strong>NavBar</strong>
+      </Text>
+    </HStack>
+  );
+}
+export default NavBar;
