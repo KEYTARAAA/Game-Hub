6985914be08504ad3fd0c2e2850835667f3fbea2
KITARA STEWART <18241476@studentmail.ul.ie>
2024-02-27 17:07:53 +0000

    Reverse Order

---
+++ src/components/ReverseOrderSwitch.tsx
@@ -0,0 +1,21 @@
+import { HStack, Switch, Text } from "@chakra-ui/react";
+import { useState } from "react";
+interface Props {
+  reverse: boolean;
+  onToggle: (reverse: boolean) => void;
+}
+function ReverseOrderSwitch({ reverse, onToggle }: Props) {
+  return (
+    <HStack>
+      <Switch
+        isChecked={reverse}
+        onChange={() => onToggle(!reverse)}
+        colorScheme="green"
+      />
+      <Text whiteSpace="nowrap">
+        <strong>Reverse Order</strong>
+      </Text>
+    </HStack>
+  );
+}
+export default ReverseOrderSwitch;
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,5 +1,14 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
+import ReverseOrderSwitch from "./ReverseOrderSwitch";
+import { useRef, useState } from "react";
 
 interface Props {
   onSelectSortOrder: (order: string) => void;
@@ -10,34 +19,56 @@ interface SortOrders {
   label: string;
 }
 function SortSelector({ onSelectSortOrder, selectedSortOrder }: Props) {
-  const sortOrders: SortOrders[] = [
+  const sortOrders = useRef<SortOrders[]>([
     { value: "", label: "Relevance" },
     { value: "-added", label: "Date added" },
     { value: "name", label: "Name" },
     { value: "-release", label: "Release Date" },
     { value: "-metacritic", label: "Popularity" },
     { value: "-rating", label: "Average Rating" },
-  ];
-  //if (error) return null;
+  ]);
+
+  const reverseOrder = () => {
+    sortOrders.current.map((sortOrder) => {
+      sortOrder.value = reverseOrderString(sortOrder.value);
+    });
+  };
+
+  const reverseOrderString = (order: string) => {
+    return order.charAt(0) === "-" ? order.slice(1) : "-" + order;
+  };
+  const [reverse, setReverse] = useState(false);
   return (
-    <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order By:{" "}
-        {sortOrders.find((order) => order.value === selectedSortOrder)?.label ||
-          "Relevance"}
-      </MenuButton>
-      <MenuList>
-        {sortOrders.map((order) => (
-          <MenuItem
-            key={order.value}
-            value={order.value}
-            onClick={() => onSelectSortOrder(order.value)}
-          >
-            {order.label}
-          </MenuItem>
-        ))}
-      </MenuList>
-    </Menu>
+    <HStack>
+      <Menu>
+        <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+          Order By:{" "}
+          {sortOrders.current.find((order) => order.value === selectedSortOrder)
+            ?.label || "Relevance"}
+        </MenuButton>
+        <MenuList>
+          {sortOrders.current.map((order) => (
+            <MenuItem
+              key={order.value}
+              value={order.value}
+              onClick={() => {
+                onSelectSortOrder(order.value);
+              }}
+            >
+              {order.label}
+            </MenuItem>
+          ))}
+        </MenuList>
+      </Menu>
+      <ReverseOrderSwitch
+        reverse={reverse}
+        onToggle={(reverse) => {
+          setReverse(reverse);
+          reverseOrder();
+          onSelectSortOrder(reverseOrderString(selectedSortOrder));
+        }}
+      />
+    </HStack>
   );
 }
 export default SortSelector;
